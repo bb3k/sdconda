a3b474d39895d943f40e23d988c3347a22b23842
bb3k <bitboy3000@gmail.com>
2023-05-17 01:41:49 +0800

    Update sdconda.sh

--- sdconda.sh
+++ sdconda.sh
@@ -5,5 +5,8 @@ git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd stable-diffusion-webui
 cd models/Stable-diffusion
 wget https://civitai.com/api/download/models/57618 -o CounterfeitV30_v30.safetensors
-cd ../..
+cd ..
+cd VAE
+wget https://huggingface.co/hakurei/waifu-diffusion-v1-4/blob/main/vae/kl-f8-anime2.ckpt
+cd ..
 python launch.py --xformers --share  --no-half --no-half-vae
